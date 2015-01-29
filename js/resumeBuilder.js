64b8429b441de4b37c331469b2ca2e44c2ab058f
daniel <deepakswami07@gmail.com>
2015-01-29 12:56:32 -0600

    jquery added to name and profession

---
+++ js/resumeBuilder.js
@@ -0,0 +1,28 @@
+
+var bio = {
+	name: "Daniel Deepak",
+	role: "Web Developer",
+	contact: {
+		mobile: "848-565-4893",
+		email: "jdeepakswami07@gmail.com",
+        github: "deepakswami07",
+        twitter: "@ddeepak",
+        location: "NYC"
+	    },
+	pic: "images/fry.jpg",
+	welcomeMessage: "lorem ipsum dolor sit amet etc etc etc.",
+	skills: ["Designer", "HTML", "CSS", "jQuery", "JavaScript"]
+}
+
+var name = bio.name;
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = bio.role;
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var mobile = bio.contact.mobile;
+var formattedMobile = HTMLmobile.replace("%data%", mobile);
+$("#topContacts").append(formattedMobile);
